68e123aed0c9fddf65ad54bd9fabc21feb151603
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-08 22:01:28 +0300

    Change: Dashboard to hold placeholder elements.

--- app/src/main/java/com/example/recipeapplication/ui/screens/Dashboard.kt
+++ app/src/main/java/com/example/recipeapplication/ui/screens/Dashboard.kt
@@ -2,9 +2,10 @@ package com.example.recipeapplication.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun DashBoard() {
+fun Dashboard(navController: NavController) {
     Text(text = "Dashboard page")
 
 }
