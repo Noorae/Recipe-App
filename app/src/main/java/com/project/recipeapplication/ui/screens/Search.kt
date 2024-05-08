b271627aab281b05ac4dd3cf637cc4d5873e4056
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-08 22:20:05 +0300

    Add: SearchBar to Search Screen.

---
+++ app/src/main/java/com/project/recipeapplication/ui/screens/Search.kt
@@ -0,0 +1,41 @@
+package com.project.recipeapplication.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Search(navController: NavController) {
+    var searchQuery by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Search page")
+        SearchBar(
+            query = searchQuery ,
+            onQueryChange = { newQuery -> searchQuery = newQuery },
+            onSearch = {/*TODO*/},
+            active = false,
+            onActiveChange = {/*TODO*/}) {
+
+        }
+
+    }
+
+}
