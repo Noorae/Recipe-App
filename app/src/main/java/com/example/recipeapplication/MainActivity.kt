88212db94def0ddd902f785dcd3fbc1510dc9927
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-08 21:59:18 +0300

    Update: MainActivity view.

--- app/src/main/java/com/example/recipeapplication/MainActivity.kt
+++ app/src/main/java/com/example/recipeapplication/MainActivity.kt
@@ -10,7 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.recipeapplication.ui.screens.DashBoard
+import com.example.recipeapplication.ui.navigation.BottomNavBar
+import com.example.recipeapplication.ui.screens.Dashboard
 import com.example.recipeapplication.ui.theme.RecipeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DashBoard()
+                    BottomNavBar()
                 }
             }
         }
