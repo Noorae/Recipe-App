b35b21fd796353276cbc738693747d3426b8a172
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-08 22:01:45 +0300

    Change: Recipes screen to hold placeholder elements.

--- app/src/main/java/com/example/recipeapplication/ui/screens/Recipes.kt
+++ app/src/main/java/com/example/recipeapplication/ui/screens/Recipes.kt
@@ -1,8 +1,26 @@
 package com.example.recipeapplication.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun Recipes() {
+fun Recipes(navController: NavController) {
+
+    val mockRecipeList = listOf("Pasta", "Ice Cream", "Coffee", )
+
+    LazyColumn {
+        items(mockRecipeList) { recipe ->
+            TextButton(
+                onClick = { /*TODO*/ },
+                ) { Text(text = recipe.toString())}
+
+        }
+    }
 
 }
