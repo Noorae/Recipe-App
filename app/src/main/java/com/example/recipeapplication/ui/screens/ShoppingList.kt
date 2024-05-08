5ed5855f065f95dd2ec6a79e529a7b0ce151e15d
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-08 22:02:27 +0300

    Change: ShoppingList screen to hold placeholder elements.

--- app/src/main/java/com/example/recipeapplication/ui/screens/ShoppingList.kt
+++ app/src/main/java/com/example/recipeapplication/ui/screens/ShoppingList.kt
@@ -1,9 +1,10 @@
 package com.example.recipeapplication.ui.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun ShoppingList(navController: NavController) {
-
+    Text(text = "Shopping list page")
 }
