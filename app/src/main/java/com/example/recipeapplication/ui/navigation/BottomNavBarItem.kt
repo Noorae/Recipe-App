94877165adbfd7d1c350ce8fc9f27690681e5e01
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-08 22:00:06 +0300

    Add: BottomNavBarItem data class.

--- app/src/main/java/com/example/recipeapplication/ui/navigation/BottomNavBarItem.kt
+++ app/src/main/java/com/example/recipeapplication/ui/navigation/BottomNavBarItem.kt
@@ -4,6 +4,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.ui.graphics.vector.ImageVector
 
-data class BottomNavBarItem(val label : String = "") {
-
-}
+data class BottomNavBarItem(
+    val label : String = "",
+    val selectedIcon: ImageVector,
+    val unSelectedIcon : ImageVector,
+    val route : String = ""
+)
