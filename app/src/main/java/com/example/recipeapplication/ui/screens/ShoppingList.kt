dc160b023dcbd56fec7a97d8b7afa4769fcc90cf
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-08 21:04:10 +0300

    Add: ShoppingList.kt screen.

---
+++ app/src/main/java/com/example/recipeapplication/ui/screens/ShoppingList.kt
@@ -0,0 +1,9 @@
+package com.example.recipeapplication.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ShoppingList(navController: NavController) {
+
+}
