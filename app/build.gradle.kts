d5b5808e3d87325dcff211a8c0b43a08323ad57d
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-15 00:27:06 +0300

    Add: Dependencies to build.gradle.kts file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,10 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.navigation.compose)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.5.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
 }
