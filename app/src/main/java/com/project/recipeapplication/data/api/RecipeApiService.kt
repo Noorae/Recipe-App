1b09ad2a311920b12459284f0892ad294019b9db
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-15 16:03:19 +0300

    Add: RecipeApiService interface to data/api for recipe fetching.

---
+++ app/src/main/java/com/project/recipeapplication/data/api/RecipeApiService.kt
@@ -0,0 +1,15 @@
+package com.project.recipeapplication.data.api
+
+import com.project.recipeapplication.data.model.ResponseData
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RecipeApiService {
+    @GET("recipes/complexSearch")
+    suspend fun searchRecipes(
+        @Query("query") query: String,
+        @Query("apiKey") apiKey: String,
+        @Query("number") number: Int,
+        @Query("instructionsRequired") instructionsRequired: Boolean
+    ): ResponseData
+}
