080061864f887f76728bb59c0592ef2f14ca7e58
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-16 20:59:08 +0300

    Add: Database connection initialization.

--- app/src/main/java/com/project/recipeapplication/MainActivity.kt
+++ app/src/main/java/com/project/recipeapplication/MainActivity.kt
@@ -7,12 +7,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.room.Room
+import com.project.recipeapplication.data.local.AppDatabase
 import com.project.recipeapplication.ui.navigation.BottomNavBar
 import com.project.recipeapplication.ui.theme.RecipeApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        lateinit var database: AppDatabase
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "recipe-database"
+        ).build()
         setContent {
             RecipeApplicationTheme {
                 // A surface container using the 'background' color from the theme
