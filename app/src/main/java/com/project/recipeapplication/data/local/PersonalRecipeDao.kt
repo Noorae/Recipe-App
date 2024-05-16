b32f6e6bad4974384ff61d299ecb127159ebd8ad
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-16 18:14:43 +0300

    Add: Functions to delete and add new recipes to PersonalRecipeDao.kt.

--- app/src/main/java/com/project/recipeapplication/data/local/PersonalRecipeDao.kt
+++ app/src/main/java/com/project/recipeapplication/data/local/PersonalRecipeDao.kt
@@ -1,12 +1,22 @@
 package com.project.recipeapplication.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Upsert
 import com.project.recipeapplication.data.model.PersonalRecipe
 
 @Dao
 interface PersonalRecipeDao {
     @Query("SELECT * FROM recipes")
     suspend fun getAll(): List<PersonalRecipe>
+
+    @Upsert
+    suspend fun upsertRecipe(recipe: PersonalRecipe)
+
+    @Delete
+    suspend fun deleteRecipe(recipe: PersonalRecipe)
+
+
 }
