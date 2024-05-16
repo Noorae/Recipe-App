2f314372840c824f9a5c0924a1149d7039251bdf
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-16 13:17:27 +0300

    Add: State management of searchQuery and recipelist to RecipesViewModel.

--- app/src/main/java/com/project/recipeapplication/viewModel/RecipesViewModel.kt
+++ app/src/main/java/com/project/recipeapplication/viewModel/RecipesViewModel.kt
@@ -1,6 +1,9 @@
 package com.project.recipeapplication.viewModel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,10 +16,18 @@ class RecipesViewModel : ViewModel() {
     private val repository = RecipeRepository()
     private val _results : SnapshotStateList<Recipe> = mutableStateListOf<Recipe>()
     val recipes : List<Recipe> get() = _results
+    var searchQuery by mutableStateOf("")
 
-    fun fetchRecipes(searchQuery : String) {
+
+    //function to take a new searchQuery
+    fun updateSearchQuery(newQuery: String) {
+        searchQuery = newQuery
+    }
+
+    fun fetchRecipes() {
         val apiKey = BuildConfig.API_KEY
 
+        //TODO change hard coded parameters into variables
         viewModelScope.launch {
             val recipeList = repository.fetchRecipes(
                 searchQuery,
@@ -24,8 +35,11 @@ class RecipesViewModel : ViewModel() {
                 3,
                 true
             )
-
+            //empty the search results after every search
+            _results.clear()
+            //populate the results with new fetched recipes
             _results.addAll(recipeList)
+            //TODO remove debugging logs
             println(_results.joinToString(separator = "\n") { it.toString() })
         }
 
