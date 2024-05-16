cab4d2180edd8464e45d1c6b4c5bb439feaa28e9
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-16 18:05:38 +0300

    Add: PersonalRecipe data class for database.

---
+++ app/src/main/java/com/project/recipeapplication/data/model/PersonalRecipe.kt
@@ -0,0 +1,13 @@
+package com.project.recipeapplication.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipes")
+data class PersonalRecipe(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val instructions: String,
+    val imagePath: String
+) {
+}
