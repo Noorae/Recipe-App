95ef405567295b2e53f3014f6feb2281b6cc0a27
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-16 21:28:43 +0300

    Add: LazyColumn to display recipes from the database.

--- app/src/main/java/com/project/recipeapplication/ui/screens/Recipes.kt
+++ app/src/main/java/com/project/recipeapplication/ui/screens/Recipes.kt
@@ -1,24 +1,84 @@
 package com.project.recipeapplication.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.project.recipeapplication.R
+import com.project.recipeapplication.data.model.PersonalRecipe
+import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 
 @Composable
-fun Recipes(navController: NavController) {
+fun Recipes(navController: NavController, viewModel: PersonalRecipeViewModel = viewModel()) {
+    val recipeState = viewModel.personalRecipes.collectAsState()
+    val recipes = recipeState.value
 
-    val mockRecipeList = listOf("Pasta", "Ice Cream", "Coffee", )
 
-    LazyColumn {
-        items(mockRecipeList) { recipe ->
-            TextButton(
-                onClick = { /*TODO*/ },
-                ) { Text(text = recipe.toString())}
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LazyColumn {
+            items(recipes) { recipe ->
+                Card(
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant,),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp, vertical = 8.dp))
+                {
+                    Row(modifier = Modifier.padding(8.dp)) {
+                        Image(
+                            painter = painterResource(id = R.drawable.placeholder),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(15.dp))
+                        )
 
+                        Text(text = recipe.title,
+                                modifier = Modifier.padding(10.dp))
+
+                        IconButton(onClick = { /*TODO*/ }) {
+
+                        }
+
+                    }
+                    Text(text = "description of recipe", modifier = Modifier.padding(8.dp))
+
+                }
+
+            }
+        }
+        Button(onClick = { /*TODO*/ }) {
+          Text(text = "add to database")
         }
     }
+    
+    
 
 }
