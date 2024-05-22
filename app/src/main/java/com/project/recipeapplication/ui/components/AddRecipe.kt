7c5e729acf192488e396386b3536e933f92545cc
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-22 20:05:24 +0300

    Add: AddRecipe, AddRecipeField composables to be used to adding recipes to database.

---
+++ app/src/main/java/com/project/recipeapplication/ui/components/AddRecipe.kt
@@ -0,0 +1,9 @@
+package com.project.recipeapplication.ui.components
+
+import androidx.compose.runtime.Composable
+
+// TODO composable to add personal recipes into the database
+@Composable
+fun AddRecipe() {
+
+}
---
+++ app/src/main/java/com/project/recipeapplication/ui/components/AddRecipeField.kt
@@ -0,0 +1,9 @@
+package com.project.recipeapplication.ui.components
+
+import androidx.compose.runtime.Composable
+
+//TODO implement a field to display input fields for AddRecipe
+@Composable
+fun AddRecipeField() {
+
+}
