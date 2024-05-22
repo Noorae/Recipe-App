d7667c1f3af5d4e26bd0eee98a8b4ff9381b9ab3
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-22 22:29:48 +0300

    Add: TextField test implementation to AddRecipe composable.

--- app/src/main/java/com/project/recipeapplication/ui/components/AddRecipe.kt
+++ app/src/main/java/com/project/recipeapplication/ui/components/AddRecipe.kt
@@ -1,9 +1,37 @@
 package com.project.recipeapplication.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 
 // TODO composable to add personal recipes into the database
 @Composable
-fun AddRecipe() {
+fun AddRecipe(navController: NavController, viewModel: PersonalRecipeViewModel = viewModel()) {
+    Column {
+        Text(text = "this is the AddRecipe Composable to be implemented")
+        TextField(
+            value = viewModel.recipeTitle,
+            onValueChange = { viewModel.recipeTitle = it},
+            label = { Text("Recipe name")},
+            )
+        TextField(
+            value = viewModel.recipeInstructions,
+            onValueChange = {viewModel.recipeInstructions = it},
+            label = { Text("Recipe instructions")},
+            )
+        TextButton(onClick = {
+            viewModel.collectRecipeData()
+            navController.navigate("recipes")
+        }) {
+            Text("Save")
+        }
+        
+    }
+    
 
 }
