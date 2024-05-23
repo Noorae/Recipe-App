beb348a104cda018a4a4c0cdd870da7487b93e9a
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-23 20:34:48 +0300

    Add: Heading composable to ShoppingList.kt.

--- app/src/main/java/com/project/recipeapplication/ui/screens/ShoppingList.kt
+++ app/src/main/java/com/project/recipeapplication/ui/screens/ShoppingList.kt
@@ -1,10 +1,36 @@
 package com.project.recipeapplication.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun ShoppingList(navController: NavController) {
-    Text(text = "Shopping list page")
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(modifier = Modifier
+            .align(Alignment.Start)
+            .padding(start = 16.dp, top = 20.dp),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            text = "Shopping List"
+        )
+        Divider()
+    }
+
 }
