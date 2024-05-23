44a17ea3df915f09ba8b46d537f708e7fb41b26c
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-23 22:23:32 +0300

    Change: Recipes screen to show correct composables based on tab index.

--- app/src/main/java/com/project/recipeapplication/ui/screens/Recipes.kt
+++ app/src/main/java/com/project/recipeapplication/ui/screens/Recipes.kt
@@ -2,6 +2,7 @@ package com.project.recipeapplication.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +12,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
@@ -25,6 +30,11 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,15 +47,21 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.project.recipeapplication.R
 import com.project.recipeapplication.data.model.PersonalRecipe
+import com.project.recipeapplication.ui.components.RecipeList
+import com.project.recipeapplication.ui.components.RecipesTabs
 import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 
 @Composable
 fun Recipes(navController: NavController, viewModel: PersonalRecipeViewModel = viewModel()) {
     val recipeState = viewModel.personalRecipes.collectAsState()
-    val recipes = recipeState.value
+    val personalRecipes = recipeState.value
+
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
 
 
-    Column(modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 16.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -56,28 +72,29 @@ fun Recipes(navController: NavController, viewModel: PersonalRecipeViewModel = v
             fontWeight = FontWeight.Bold,
             text = "Recipes"
         )
+        RecipesTabs(
+            selectedTabIndex = selectedTabIndex,
+            onTabSelected = { index -> selectedTabIndex = index }
+        )
         Divider()
-        LazyColumn(modifier = Modifier.weight(1f).padding(5.dp)) {
-            items(recipes) { recipe ->
-                ListItem(
-                    modifier = Modifier.padding(vertical = 8.dp).clip(RoundedCornerShape(8.dp)),
-                    colors = ListItemDefaults.colors(MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)),
-                    headlineContent = { Text(recipe.title)},
-                    leadingContent = {
-                        Image(painter = painterResource(id = R.drawable.placeholder),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.padding(vertical = 8.dp)
-                            .size(100.dp)
-                            .clip(RoundedCornerShape(10.dp)))
-                    }
-                )
 
+        Box(modifier = Modifier.fillMaxSize()) {
+            when (selectedTabIndex) {
+                0 -> RecipeList(recipes = personalRecipes)
+                1 -> Text(text = "Apin favoritet")
             }
+            
+            ExtendedFloatingActionButton(
+                text = { Text(text = "new recipe") },
+                icon = { Icon(Icons.Filled.Create, "Extended floating action button") },
+                onClick = { navController.navigate("addRecipe") },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomEnd))
+
         }
-        Button(onClick = { navController.navigate("addRecipe")}, modifier = Modifier.padding(bottom = 20.dp)) {
-          Text(text = "Add new recipe")
-        }
+
+        
     }
     
 
