3718d0dd18c8d0134dba02a5866814c0cff426b6
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-23 20:34:02 +0300

    Change: Search.kt to use Listitems instead of Card composables.

--- app/src/main/java/com/project/recipeapplication/ui/screens/Search.kt
+++ app/src/main/java/com/project/recipeapplication/ui/screens/Search.kt
@@ -1,5 +1,6 @@
 package com.project.recipeapplication.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,10 +12,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +32,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.project.recipeapplication.ui.components.RecipeSearchBar
+import com.project.recipeapplication.ui.theme.RecipeApplicationTheme
 import com.project.recipeapplication.viewModel.ApiRecipesViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,52 +41,54 @@ fun Search(navController: NavController, viewModel: ApiRecipesViewModel = viewMo
     val searchQuery = viewModel.searchQuery
     val recipes = viewModel.recipes
 
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Text(modifier = Modifier
-            .align(Alignment.Start)
-            .padding(start = 16.dp, top = 20.dp),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            text = "Search recipes"
-        )
-        RecipeSearchBar(
-            searchQuery = searchQuery,
-            onQueryChange = { newQuery -> viewModel.updateSearchQuery(newQuery) },
-            onSearch = { viewModel.fetchRecipes() }
-        )
-        LazyColumn {
-            items(recipes) { recipe ->
-                Card(
-                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant,),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp, vertical = 8.dp))
-                {
-                    Row(modifier = Modifier.padding(8.dp)) {
-                        AsyncImage(
-                            model = recipe.image,
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier.clip(RoundedCornerShape(15.dp))
-                        )
-                        Text(text = recipe.title,
-                            modifier = Modifier.padding(10.dp))
-                        IconButton(onClick = { /*TODO*/ }) {
-                            
+            //TODO Add heading to its own composable
+            Text(
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(start = 16.dp, top = 20.dp),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                text = "Search recipes"
+            )
+            Divider()
+            RecipeSearchBar(
+                searchQuery = searchQuery,
+                onQueryChange = { newQuery -> viewModel.updateSearchQuery(newQuery) },
+                onSearch = { viewModel.fetchRecipes() }
+            )
+            LazyColumn(modifier = Modifier
+                .weight(1f)
+                .padding(5.dp)) {
+                items(recipes) { recipe ->
+                    ListItem(
+                        modifier = Modifier
+                            .padding(vertical = 8.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        colors = ListItemDefaults.colors(MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)),
+                        headlineContent = { Text(recipe.title) },
+                        leadingContent = {
+                            AsyncImage(
+                                model = recipe.image,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier.clip(RoundedCornerShape(10.dp))
+                            )
                         }
-                    }
+                    )
+
 
                 }
-
             }
+
         }
 
-    }
 
 }
