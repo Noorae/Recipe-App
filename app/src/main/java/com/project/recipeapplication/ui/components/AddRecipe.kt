2aa097640ca1f312121d91ad39468c1bc778720d
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-23 22:42:07 +0300

    Add: Heading and alignment modifications to AddRecipe.kt

--- app/src/main/java/com/project/recipeapplication/ui/components/AddRecipe.kt
+++ app/src/main/java/com/project/recipeapplication/ui/components/AddRecipe.kt
@@ -1,10 +1,20 @@
 package com.project.recipeapplication.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
@@ -12,19 +22,36 @@ import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 // TODO composable to add personal recipes into the database
 @Composable
 fun AddRecipe(navController: NavController, viewModel: PersonalRecipeViewModel = viewModel()) {
-    Column {
-        Text(text = "this is the AddRecipe Composable to be implemented")
-        TextField(
-            value = viewModel.recipeTitle,
-            onValueChange = { viewModel.recipeTitle = it},
-            label = { Text("Recipe name")},
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(modifier = Modifier
+            .align(Alignment.Start)
+            .padding(start = 16.dp, top = 20.dp),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            text = "New recipe info"
+        )
+        Divider()
+        Column(modifier= Modifier.padding(vertical = 10.dp)) {
+            TextField(
+                value = viewModel.recipeTitle,
+                onValueChange = { viewModel.recipeTitle = it},
+                label = { Text("Recipe name")},
             )
-        TextField(
-            value = viewModel.recipeInstructions,
-            onValueChange = {viewModel.recipeInstructions = it},
-            label = { Text("Recipe instructions")},
+            TextField(
+                value = viewModel.recipeInstructions,
+                onValueChange = {viewModel.recipeInstructions = it},
+                label = { Text("Recipe instructions")},
             )
-        TextButton(onClick = {
+
+        }
+
+        Button(onClick = {
             viewModel.collectRecipeData()
             navController.navigate("recipes")
         }) {
@@ -32,6 +59,5 @@ fun AddRecipe(navController: NavController, viewModel: PersonalRecipeViewModel =
         }
         
     }
-    
 
 }
