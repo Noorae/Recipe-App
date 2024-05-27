12117fc953b7a61653f4557eaa0de3c415c49e9f
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-27 22:44:19 +0300

    Add: CustomTopBar.kt composable to be rendered on top of screens.

---
+++ app/src/main/java/com/project/recipeapplication/ui/components/CustomTopBar.kt
@@ -0,0 +1,36 @@
+package com.project.recipeapplication.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopBar(
+    title: String,
+    navController: NavController,
+    showBackButton: Boolean = false
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            if (showBackButton) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Arrow to navigate back"
+                    )
+
+                }
+            }
+        }
+
+    )
+
+}
