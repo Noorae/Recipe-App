ee6312d6673ff6baa2546630a8e01f7da2f23a26
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-28 18:59:19 +0300

    Add: Converters functions to transform data to database and out of database.

---
+++ app/src/main/java/com/project/recipeapplication/data/local/Converters.kt
@@ -0,0 +1,37 @@
+package com.project.recipeapplication.data.local
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.project.recipeapplication.data.model.database.Ingredient
+import com.project.recipeapplication.data.model.database.InstructionStep
+
+class Converters {
+    @TypeConverter
+    fun fromIngredientList(value: List<Ingredient>): String {
+        val gson = Gson()
+        val type = object : TypeToken<List<Ingredient>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toIngredientList(value: String): List<Ingredient> {
+        val gson = Gson()
+        val type = object : TypeToken<List<Ingredient>>() {}.type
+        return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromInstructionStepList(value: List<InstructionStep>): String {
+        val gson = Gson()
+        val type = object : TypeToken<List<InstructionStep>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toInstructionStepList(value: String): List<InstructionStep> {
+        val gson = Gson()
+        val type = object : TypeToken<List<InstructionStep>>() {}.type
+        return gson.fromJson(value, type)
+    }
+}
