7bccace9f6c5d5720c307ee4fb97ea1e04eba0ba
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-28 18:57:18 +0300

    Add: New database functions to DatabaseDao to fetch data.

--- app/src/main/java/com/project/recipeapplication/data/local/PersonalRecipeDao.kt
+++ app/src/main/java/com/project/recipeapplication/data/local/PersonalRecipeDao.kt
@@ -4,19 +4,43 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
-import com.project.recipeapplication.data.model.PersonalRecipe
+import com.project.recipeapplication.data.model.database.Ingredient
+import com.project.recipeapplication.data.model.database.InstructionStep
+import com.project.recipeapplication.data.model.database.PersonalRecipe
+import com.project.recipeapplication.data.model.database.RecipeSummary
+import com.project.recipeapplication.data.model.database.RecipeWithFullData
+import com.project.recipeapplication.data.model.database.Tag
 
 @Dao
 interface PersonalRecipeDao {
-    @Query("SELECT * FROM recipes")
-    suspend fun getAll(): List<PersonalRecipe>
+    //Select summarized list of recipes
+    @Query("SELECT id, title, imagePath, mealType, isFavorite FROM recipes")
+    suspend fun getAll(): List<RecipeSummary>
 
-    @Upsert
-    suspend fun upsertRecipe(recipe: PersonalRecipe)
+    //Add new recipe
+    @Insert
+    suspend fun insertRecipe(recipe: PersonalRecipe): Long
 
+    //add ingredients list
+    @Insert
+    suspend fun insertIngredients(ingredients: List<Ingredient>)
+
+    //add instruction step list
+    @Insert
+    suspend fun insertInstructionSteps(instructionSteps: List<InstructionStep>)
+
+    // add tag list
+    @Insert
+    suspend fun insertTags(tags: List<Tag>)
     @Delete
     suspend fun deleteRecipe(recipe: PersonalRecipe)
 
+    @Transaction
+    @Query("SELECT * FROM recipes WHERE id = :recipeId")
+    suspend fun getRecipeWithFullData(recipeId: Int): RecipeWithFullData
 
+    @Query("DELETE FROM recipes WHERE id = :id")
+    suspend fun deleteRecipeById(id: Int)
 }
