6a87ac4893da7da8db90455ca794579d416f19cf
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 00:03:04 +0300

    Add: RecipeSection, IngredientSection,InstructionSection and TagSection to AddRecipe view.

--- app/src/main/java/com/project/recipeapplication/ui/components/AddRecipe.kt
+++ app/src/main/java/com/project/recipeapplication/ui/components/AddRecipe.kt
@@ -1,17 +1,31 @@
 package com.project.recipeapplication.ui.components
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,11 +35,13 @@ import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 
 // TODO composable to add personal recipes into the database
 @Composable
-fun AddRecipe(navController: NavController, viewModel: PersonalRecipeViewModel = viewModel()) {
+fun AddRecipe(navController: NavController, viewModel: PersonalRecipeViewModel) {
+    val scrollState = rememberScrollState()
 
     Column(modifier = Modifier
         .fillMaxSize()
-        .padding(horizontal = 16.dp),
+        .padding(horizontal = 16.dp)
+        .verticalScroll(scrollState),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -36,16 +52,15 @@ fun AddRecipe(navController: NavController, viewModel: PersonalRecipeViewModel =
         )
         Divider()
         Column(modifier= Modifier.padding(vertical = 10.dp)) {
-            TextField(
-                value = viewModel.recipeTitle,
-                onValueChange = { viewModel.recipeTitle = it},
-                label = { Text("Recipe name")},
-            )
-            TextField(
-                value = viewModel.recipeInstructions,
-                onValueChange = {viewModel.recipeInstructions = it},
-                label = { Text("Recipe instructions")},
-            )
+            
+            RecipeSection(viewModel = viewModel)
+            Divider()
+            IngredientsSection(viewModel = viewModel)
+            Divider()
+            InstructionsSection(viewModel = viewModel)
+            Divider()
+            TagSection(viewModel = viewModel)
+
 
         }
 
