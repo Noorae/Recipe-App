4a5ffe0152414cab30908ec4d6be7c8ca38a63d4
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-28 18:56:05 +0300

    Add: Temprorary TrailingContent icon to RecipeList ListItems to delete recipes.

--- app/src/main/java/com/project/recipeapplication/ui/components/RecipeList.kt
+++ app/src/main/java/com/project/recipeapplication/ui/components/RecipeList.kt
@@ -6,22 +6,29 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.project.recipeapplication.R
-import com.project.recipeapplication.data.model.PersonalRecipe
+import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 
 @Composable
-fun RecipeList(recipes: List<PersonalRecipe>, modifier: Modifier = Modifier) {
+fun RecipeList(modifier: Modifier = Modifier, viewModel: PersonalRecipeViewModel) {
+    val recipeState = viewModel.personalRecipes.collectAsState()
+    val recipes = recipeState.value
 
     LazyColumn(modifier = Modifier
         .padding(5.dp)) {
@@ -40,8 +47,16 @@ fun RecipeList(recipes: List<PersonalRecipe>, modifier: Modifier = Modifier) {
                             .padding(vertical = 8.dp)
                             .size(100.dp)
                             .clip(RoundedCornerShape(10.dp)))
-                }
-            )
+                },
+                trailingContent = {
+                    IconButton(
+                        onClick = {
+                            println("deletebutton with recipe id ${recipe.id} clicked")
+                            viewModel.deleteRecipeById(recipe.id)
+
+                        }
+
+                ) {Icon(imageVector = Icons.Filled.Delete, contentDescription = "Icon to display more options")}})
 
         }
     }
