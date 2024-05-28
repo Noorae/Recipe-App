5f2a6105056f803821ddb6d09f4645a4f2e30618
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 00:00:25 +0300

    Add: New composable RecipeSection to be used on AddRecipe view.

---
+++ app/src/main/java/com/project/recipeapplication/ui/components/RecipeSection.kt
@@ -0,0 +1,68 @@
+package com.project.recipeapplication.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
+
+@Composable
+fun RecipeSection(viewModel: PersonalRecipeViewModel) {
+    Column {
+        Row {
+            Box(modifier = Modifier
+                .weight(1f)
+                .padding(8.dp)
+                .background(Color.Red)) {
+                Text(text = "test")
+            }
+            Box(modifier = Modifier
+                .weight(1f)
+                .padding(8.dp)) {
+                Column {
+                    OutlinedTextField(
+                        value = viewModel.recipeTitle,
+                        onValueChange = { viewModel.recipeTitle = it},
+                        label = { Text("Recipe name") },
+                        modifier = Modifier.padding(5.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = if (viewModel.recipeServingSize == 0){
+                            ""} else { viewModel.recipeServingSize.toString()}
+                        ,
+                        onValueChange = { viewModel.recipeServingSize = it.toIntOrNull() ?: 0 },
+                        label = { Text("Servings") },
+                        modifier = Modifier.padding(5.dp)
+                    )
+
+                }
+
+            }
+        }
+        Divider()
+        //  TODO ADD MISSING VALUES
+        OutlinedTextField(
+            value = viewModel.recipeMealType,
+            onValueChange = { viewModel.recipeMealType = it},
+            label = { Text("Meal Type") },
+            modifier = Modifier.padding(5.dp)
+        )
+        OutlinedTextField(
+            value = viewModel.recipeReady,
+            onValueChange = { viewModel.recipeReady = it},
+            label = { Text("Cooking time") },
+            modifier = Modifier.padding(6.dp)
+        )
+
+    }
+
+}
