1534dc1d7238957534ea104063d69e671d6d7c9f
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 14:05:13 +0300

    Add: Entity ShoppingItem to AppDatabase.kt.

--- app/src/main/java/com/project/recipeapplication/data/local/AppDatabase.kt
+++ app/src/main/java/com/project/recipeapplication/data/local/AppDatabase.kt
@@ -6,9 +6,10 @@ import androidx.room.TypeConverters
 import com.project.recipeapplication.data.model.database.Ingredient
 import com.project.recipeapplication.data.model.database.InstructionStep
 import com.project.recipeapplication.data.model.database.PersonalRecipe
+import com.project.recipeapplication.data.model.database.ShoppingItem
 import com.project.recipeapplication.data.model.database.Tag
 
-@Database(entities = [PersonalRecipe::class, Ingredient::class, InstructionStep::class, Tag::class], version = 2)
+@Database(entities = [PersonalRecipe::class, Ingredient::class, InstructionStep::class, Tag::class, ShoppingItem::class], version = 3)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun personalRecipeDao(): PersonalRecipeDao
