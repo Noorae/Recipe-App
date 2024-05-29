a1501902ab35698a4b724577ef7f56498f01fc76
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 14:08:57 +0300

    Add: Dialog, ShoppingItems composable,Floating action button on ShoppingList screen.kt.

--- app/src/main/java/com/project/recipeapplication/ui/screens/ShoppingList.kt
+++ app/src/main/java/com/project/recipeapplication/ui/screens/ShoppingList.kt
@@ -1,22 +1,36 @@
 package com.project.recipeapplication.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.project.recipeapplication.data.model.database.ShoppingItem
 import com.project.recipeapplication.ui.components.CustomTopBar
+import com.project.recipeapplication.ui.components.ShoppingItemDialog
+import com.project.recipeapplication.ui.components.ShoppingItems
+import com.project.recipeapplication.viewModel.ShoppingViewModel
 
 @Composable
-fun ShoppingList(navController: NavController) {
+fun ShoppingList(navController: NavController, viewModel : ShoppingViewModel) {
+    var showDialog by remember { mutableStateOf(false) }
 
     Column(modifier = Modifier
         .fillMaxSize()
@@ -29,6 +43,29 @@ fun ShoppingList(navController: NavController) {
             navController =  navController,
         )
         Divider()
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            ShoppingItems(viewModel = viewModel)
+
+            ExtendedFloatingActionButton(
+                text = { Text(text = "new item") },
+                icon = { Icon(Icons.Filled.Create, "Extended floating action button") },
+                onClick = { showDialog = true },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomEnd))
+
+        }
+
+    }
+
+    if (showDialog) {
+        ShoppingItemDialog(
+            onDismiss = { showDialog = false },
+            onAddItem = { newItemName ->
+                viewModel.addNewItem(ShoppingItem(name = newItemName, itemChecked = false))
+            }
+        )
     }
 
 }
