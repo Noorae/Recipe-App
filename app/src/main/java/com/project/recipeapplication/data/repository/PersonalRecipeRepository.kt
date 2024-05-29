126cb987077d6d6ef0f7d57f55c9291c1343e71d
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 14:00:52 +0300

    Refactor: Add RecipeApplication as the source of database instance in function calls in PersonalRecipeRepository.kt.

--- app/src/main/java/com/project/recipeapplication/data/repository/PersonalRecipeRepository.kt
+++ app/src/main/java/com/project/recipeapplication/data/repository/PersonalRecipeRepository.kt
@@ -1,6 +1,7 @@
 package com.project.recipeapplication.data.repository
 
 import com.project.recipeapplication.MainActivity
+import com.project.recipeapplication.RecipeApplication
 import com.project.recipeapplication.data.model.database.Ingredient
 import com.project.recipeapplication.data.model.database.InstructionStep
 import com.project.recipeapplication.data.model.database.PersonalRecipe
@@ -11,7 +12,7 @@ import com.project.recipeapplication.data.model.database.Tag
 class PersonalRecipeRepository() {
 
     suspend fun fetchRecipes(): List<RecipeSummary> {
-        val recipes = MainActivity.database.personalRecipeDao().getAll()
+        val recipes = RecipeApplication.database.personalRecipeDao().getAll()
         println(recipes)
         return recipes
     }
@@ -21,18 +22,18 @@ class PersonalRecipeRepository() {
                           instructions: List<InstructionStep>,
                           tags: List<Tag>
                           ) {
-        val recipeId = MainActivity.database.personalRecipeDao().insertRecipe(recipe).toInt()
+        val recipeId = RecipeApplication.database.personalRecipeDao().insertRecipe(recipe).toInt()
         println("Successfully added recipe with id $recipeId")
 
         if (ingredients.isNotEmpty()) {
             ingredients.forEach {it.recipeId = recipeId }
             println("Successfully added ingredients to recipe with id $recipeId")
-            MainActivity.database.personalRecipeDao().insertIngredients(ingredients)
+            RecipeApplication.database.personalRecipeDao().insertIngredients(ingredients)
         } else {println("ingredients list was empty")}
         if (instructions.isNotEmpty()) {
             instructions.forEach {it.recipeId = recipeId}
             println("Successfully added instructions to recipe with id $recipeId")
-            MainActivity.database.personalRecipeDao().insertInstructionSteps(instructions)
+            RecipeApplication.database.personalRecipeDao().insertInstructionSteps(instructions)
 
         } else {
             println("instructions list was empty")
@@ -41,7 +42,7 @@ class PersonalRecipeRepository() {
         if (tags.isNotEmpty()) {
             tags.forEach {it.recipeId = recipeId}
             println("Successfully added tags with id $recipeId")
-            MainActivity.database.personalRecipeDao().insertTags(tags)
+            RecipeApplication.database.personalRecipeDao().insertTags(tags)
 
         } else {
             println("instructions list was empty")
@@ -50,10 +51,10 @@ class PersonalRecipeRepository() {
     }
 
     suspend fun getRecipeWithFullData(recipeId: Int): RecipeWithFullData {
-        return MainActivity.database.personalRecipeDao().getRecipeWithFullData(recipeId)
+        return RecipeApplication.database.personalRecipeDao().getRecipeWithFullData(recipeId)
     }
 
     suspend fun deleteRecipeById(id : Int) {
-        MainActivity.database.personalRecipeDao().deleteRecipeById(id)
+        RecipeApplication.database.personalRecipeDao().deleteRecipeById(id)
     }
 }
