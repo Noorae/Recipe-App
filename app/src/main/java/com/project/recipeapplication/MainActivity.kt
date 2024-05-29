f05e14b13cb20b3875226d71c523945e132b36c2
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 13:57:49 +0300

    Refactor: Remove database connection creaton and add viewModel instance creation to MainActivity.kt

--- app/src/main/java/com/project/recipeapplication/MainActivity.kt
+++ app/src/main/java/com/project/recipeapplication/MainActivity.kt
@@ -3,27 +3,30 @@ package com.project.recipeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Room
 import com.project.recipeapplication.data.local.AppDatabase
 import com.project.recipeapplication.ui.navigation.BottomNavBar
 import com.project.recipeapplication.ui.theme.RecipeApplicationTheme
+import com.project.recipeapplication.viewModel.ApiRecipesViewModel
+import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
+import com.project.recipeapplication.viewModel.ShoppingViewModel
 
 class MainActivity : ComponentActivity() {
 
-    companion object {
-        lateinit var database: AppDatabase
-    }
+    private val apiViewModel : ApiRecipesViewModel by viewModels()
+    private val personalRecipeViewModel: PersonalRecipeViewModel by viewModels()
+    private val shoppingViewModel: ShoppingViewModel by viewModels()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        database = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java,
-            "recipe-database"
-        ).build()
         setContent {
             RecipeApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -31,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BottomNavBar()
+                    BottomNavBar(apiViewModel, personalRecipeViewModel, shoppingViewModel)
                 }
             }
         }
