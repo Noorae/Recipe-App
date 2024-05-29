b57e179547e0e4cf55153ebeb32823a8da4bb5ab
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 14:01:48 +0300

    Add: ShoppingListRepository to handle database functions on ShoppingItems.

---
+++ app/src/main/java/com/project/recipeapplication/data/repository/ShoppingListRepository.kt
@@ -0,0 +1,34 @@
+package com.project.recipeapplication.data.repository
+
+import com.project.recipeapplication.RecipeApplication
+import com.project.recipeapplication.MainActivity
+import com.project.recipeapplication.data.model.database.RecipeSummary
+import com.project.recipeapplication.data.model.database.ShoppingItem
+
+class ShoppingListRepository() {
+
+    //fetch all shoppinglist items
+    suspend fun fetchShoppingList(): List<ShoppingItem> {
+        val shoppingItems = RecipeApplication.database.personalRecipeDao().getShoppingList()
+        println(shoppingItems)
+        return shoppingItems
+    }
+
+    // add new sopping item
+    suspend fun addShoppingItem(item: ShoppingItem) {
+        RecipeApplication.database.personalRecipeDao().insertShoppingItem(item)
+
+    }
+
+    //update shopping item
+    suspend fun updateShoppingItem(item: ShoppingItem) {
+        RecipeApplication.database.personalRecipeDao().updateShoppingItem(item)
+    }
+
+    //delete shopping item
+    suspend fun removeShoppingItem(item: ShoppingItem) {
+        RecipeApplication.database.personalRecipeDao().deleteShoppingItem(item)
+    }
+
+}
+
