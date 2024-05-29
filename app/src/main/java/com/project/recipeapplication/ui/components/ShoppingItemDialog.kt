c7f6b1edaed99fb267ce4b0a7dba55613c70216a
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 13:55:49 +0300

    Add: ShoppingItemDialog.kt to add shopping item data to ShoppingList.

---
+++ app/src/main/java/com/project/recipeapplication/ui/components/ShoppingItemDialog.kt
@@ -0,0 +1,51 @@
+package com.project.recipeapplication.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShoppingItemDialog(onDismiss: () -> Unit, onAddItem: (String) -> Unit) {
+    var itemName by remember { mutableStateOf("") }
+
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = { Text("Add New Item") },
+        text = {
+            Column {
+                OutlinedTextField(
+                    value = itemName,
+                    onValueChange = { itemName = it },
+                    label = { Text("Item Name") },
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+                )
+            }
+        },
+        confirmButton = {
+            Button(onClick = {
+                if (itemName.isNotBlank()) {
+                    onAddItem(itemName)
+                    onDismiss()
+                }
+            }) {
+                Text("Add")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { onDismiss() }) {
+                Text("Cancel")
+            }
+        }
+    )
+}
