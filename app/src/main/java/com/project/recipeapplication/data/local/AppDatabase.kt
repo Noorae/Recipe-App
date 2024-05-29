a63da3a8ad141a722e979101ffe38eb62e2b115a
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-29 22:17:29 +0300

    Add: Use of ApiRecipe class to AppDatabase.kt

--- app/src/main/java/com/project/recipeapplication/data/local/AppDatabase.kt
+++ app/src/main/java/com/project/recipeapplication/data/local/AppDatabase.kt
@@ -3,14 +3,17 @@ package com.project.recipeapplication.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.project.recipeapplication.data.model.database.ApiFavoriteRecipe
 import com.project.recipeapplication.data.model.database.Ingredient
 import com.project.recipeapplication.data.model.database.InstructionStep
 import com.project.recipeapplication.data.model.database.PersonalRecipe
 import com.project.recipeapplication.data.model.database.ShoppingItem
 import com.project.recipeapplication.data.model.database.Tag
 
-@Database(entities = [PersonalRecipe::class, Ingredient::class, InstructionStep::class, Tag::class, ShoppingItem::class], version = 3)
+@Database(entities = [PersonalRecipe::class, Ingredient::class, InstructionStep::class, Tag::class, ShoppingItem::class, ApiFavoriteRecipe::class], version = 4)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun personalRecipeDao(): PersonalRecipeDao
+
+    abstract fun apiRecipeDao(): ApiRecipeDao
 }
