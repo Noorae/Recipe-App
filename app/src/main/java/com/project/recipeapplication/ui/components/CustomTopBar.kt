72c200a9e7a02af05ed7fffd12b0eee21f97d3cd
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-30 15:04:30 +0300

    Change: CustomTopBar to be used on screens other than Deashboard.kt.

--- app/src/main/java/com/project/recipeapplication/ui/components/CustomTopBar.kt
+++ app/src/main/java/com/project/recipeapplication/ui/components/CustomTopBar.kt
@@ -1,35 +1,56 @@
 package com.project.recipeapplication.ui.components
 
+
+
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.ToggleOff
+import androidx.compose.material.icons.filled.ToggleOn
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTopBar(
     title: String,
     navController: NavController,
-    showBackButton: Boolean = false
+    showBackButton: Boolean = false,
 ) {
+
     TopAppBar(
         title = { Text(text = title) },
         navigationIcon = {
             if (showBackButton) {
                 IconButton(onClick = { navController.popBackStack() }) {
                     Icon(
-                        imageVector = Icons.Filled.ArrowBack,
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "Arrow to navigate back"
                     )
 
                 }
             }
-        }
+        },
+
 
     )
 
