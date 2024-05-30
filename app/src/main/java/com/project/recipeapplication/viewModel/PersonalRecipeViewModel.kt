e01699807741c9eae4dd1887e6fd77c1d6ecc061
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-30 14:38:46 +0300

    Add: Functionality to display images from device memory.

--- app/src/main/java/com/project/recipeapplication/viewModel/PersonalRecipeViewModel.kt
+++ app/src/main/java/com/project/recipeapplication/viewModel/PersonalRecipeViewModel.kt
@@ -1,15 +1,22 @@
 package com.project.recipeapplication.viewModel
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.project.recipeapplication.BuildConfig
+import com.project.recipeapplication.data.model.api.ApiDetailedRecipe
 import com.project.recipeapplication.data.model.database.Ingredient
 import com.project.recipeapplication.data.model.database.InstructionStep
 import com.project.recipeapplication.data.model.database.PersonalRecipe
 import com.project.recipeapplication.data.model.database.RecipeSummary
+import com.project.recipeapplication.data.model.database.RecipeWithFullData
 import com.project.recipeapplication.data.model.database.Tag
 import com.project.recipeapplication.data.repository.PersonalRecipeRepository
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -21,9 +28,16 @@ class PersonalRecipeViewModel(): ViewModel() {
     private val _personalRecipes = MutableStateFlow<List<RecipeSummary>>(emptyList())
     val personalRecipes: StateFlow<List<RecipeSummary>> = _personalRecipes
 
+    //selected Personal recipe data
+    private var _selectedRecipeDetails = MutableStateFlow<RecipeWithFullData?>(null)
+    val selectedRecipeDetails: StateFlow<RecipeWithFullData?> get() = _selectedRecipeDetails
+
+    //selected id data
+    var selectedId by mutableIntStateOf(0)
+
     //recipe data
     var recipeTitle by mutableStateOf("")
-    var recipeImagePath by mutableStateOf("")
+    var recipeImagePath by mutableStateOf<String?>(null)
     var recipeReady by mutableStateOf("")
     var recipeMealType by mutableStateOf("")
     var recipeServingSize by mutableStateOf(0)
@@ -75,7 +89,7 @@ class PersonalRecipeViewModel(): ViewModel() {
 
         val newRecipe = PersonalRecipe(
             title = recipeTitle,
-            imagePath = "will be added later",
+            imagePath = recipeImagePath ?: "",
             readyInMinutes = recipeReady,
             mealType = recipeMealType,
             servingSize = recipeServingSize,
@@ -92,13 +106,26 @@ class PersonalRecipeViewModel(): ViewModel() {
     //Helper function to clean all fields
     private fun clearAllFields() {
         recipeTitle = ""
-        recipeImagePath = ""
+        recipeImagePath = null
         recipeReady = ""
         recipeMealType = ""
         recipeServingSize = 0
         recipeInstructions.clear()
         recipeTags.clear()
         recipeIngredients.clear()
+    }
+
+    //fetch selected recipe data
+    fun fetchRecipeDetails(recipeId: Int) {
+        viewModelScope.launch {
+            val details = repository.getRecipeWithFullData(recipeId)
+            _selectedRecipeDetails.value = details
+
+            println(selectedRecipeDetails)
+
+
+        }
+
     }
 
 
@@ -154,4 +181,13 @@ class PersonalRecipeViewModel(): ViewModel() {
         println(recipeTags)
     }
 
+    // Methods for permissions
+    fun setImageUri(context: Context, uri: Uri?) {
+        uri?.let {
+            context.contentResolver.takePersistableUriPermission(
+                it,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            recipeImagePath = it.toString()
+        }
+    }
 }
