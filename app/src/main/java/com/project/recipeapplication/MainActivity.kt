32802c24af92f99dcc327724711faf9a7c46e11b
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-30 15:02:55 +0300

    Add: Use of ThemePreference to MainActivity.kt to access stored data on used theme.

--- app/src/main/java/com/project/recipeapplication/MainActivity.kt
+++ app/src/main/java/com/project/recipeapplication/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Room
@@ -18,6 +21,7 @@ import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 import com.project.recipeapplication.viewModel.ShoppingViewModel
 
 class MainActivity : ComponentActivity() {
+    private lateinit var themePreferences: ThemePreference
 
     private val apiViewModel : ApiRecipesViewModel by viewModels()
     private val personalRecipeViewModel: PersonalRecipeViewModel by viewModels()
@@ -27,14 +31,21 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        themePreferences = ThemePreference(this)
+        var isDarkTheme by mutableStateOf(themePreferences.isDarkTheme())
         setContent {
-            RecipeApplicationTheme {
+            RecipeApplicationTheme(darkTheme = isDarkTheme) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BottomNavBar(apiViewModel, personalRecipeViewModel, shoppingViewModel)
+                    BottomNavBar(apiViewModel, personalRecipeViewModel, shoppingViewModel, isDarkTheme = isDarkTheme,
+                        onToggleTheme = {
+                            isDarkTheme = it
+                            themePreferences.setDarkTheme(isDarkTheme)
+                        })
                 }
             }
         }
