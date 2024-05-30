0e6845d8a8304695591bb6425c2370bc1a051e80
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-30 14:58:22 +0300

    Add: Use of recipe images or placeholder images on recipeList.kt when displaying recipe data.

--- app/src/main/java/com/project/recipeapplication/ui/components/RecipeList.kt
+++ app/src/main/java/com/project/recipeapplication/ui/components/RecipeList.kt
@@ -1,6 +1,8 @@
 package com.project.recipeapplication.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,16 +19,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.project.recipeapplication.R
 import com.project.recipeapplication.viewModel.PersonalRecipeViewModel
 
 @Composable
-fun RecipeList(modifier: Modifier = Modifier, viewModel: PersonalRecipeViewModel) {
+fun RecipeList(navController : NavController, modifier: Modifier = Modifier, viewModel: PersonalRecipeViewModel) {
     val recipeState = viewModel.personalRecipes.collectAsState()
     val recipes = recipeState.value
 
@@ -36,17 +43,39 @@ fun RecipeList(modifier: Modifier = Modifier, viewModel: PersonalRecipeViewModel
             ListItem(
                 modifier = Modifier
                     .padding(vertical = 8.dp)
-                    .clip(RoundedCornerShape(8.dp)),
+                    .clip(RoundedCornerShape(8.dp))
+                    .clickable(
+                        onClick = {
+                            viewModel.selectedId = recipe.id
+                            viewModel.fetchRecipeDetails(recipe.id)
+                            navController.navigate("personalRecipeInfo")
+                        },
+                    ),
                 colors = ListItemDefaults.colors(MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)),
                 headlineContent = { Text(recipe.title) },
                 leadingContent = {
-                    Image(painter = painterResource(id = R.drawable.placeholder),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .padding(vertical = 8.dp)
-                            .size(100.dp)
-                            .clip(RoundedCornerShape(10.dp)))
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.size(100.dp).padding(vertical = 5.dp)
+                    ) {
+                        if (recipe.imagePath.isNotEmpty() || recipe.imagePath.isNotBlank()) {
+                            // Display the image if imagePath is not empty
+                            AsyncImage(
+                                model = recipe.imagePath,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier.clip(RoundedCornerShape(10.dp))
+                            )
+                        } else {
+                            // Display placeholder image if imagePath is empty
+                            Image(
+                                painter = painterResource(id = R.drawable.placeholder),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier.clip(RoundedCornerShape(10.dp))
+                            )
+                        }
+                    }
                 },
                 trailingContent = {
                     IconButton(
