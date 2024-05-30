f83176ad6b4014335b7699e6a9c29e233da173ae
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-05-30 17:24:14 +0300

    Add: Custom colors to themes in Color.kt.

--- app/src/main/java/com/project/recipeapplication/ui/theme/Color.kt
+++ app/src/main/java/com/project/recipeapplication/ui/theme/Color.kt
@@ -2,10 +2,25 @@ package com.project.recipeapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFD0BCFF)   //tumman teeman vaalean lila
+val PurpleGrey80 = Color(0xFFCCC2DC) // tumman teeman harmaa
+val Pink80 = Color(0xFFEFB8C8) // tumman teeman pinkki
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF6650a4)   //vaalean teeman tummin
+val PurpleGrey40 = Color(0xFF625b71) //vaalean teeman harmaa
+val Pink40 = Color(0xFF7D5260) // vaalean teemun tumman pinkki
+
+val Custom1 = Color(0xFFBDFFCD)
+val Custom2 = Color(0xFFC1DBC2)
+val Custom3 = Color(0xFFF0F0B9)
+
+
+val Custom4 = Color(0xFF50A35A)
+val Custom5 = Color(0xFF64705B)
+val Custom6 = Color(0xFF7D6C52)
+
+//for actionbuttons
+val Custom7 = Color(0xFF83D889)
+
+
+
